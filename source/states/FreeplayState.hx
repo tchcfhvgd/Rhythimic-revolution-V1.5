f502529b9612d6c0d64acf06c1caf7a672b20872
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-12-15 10:21:55 +0800

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -110,7 +110,7 @@ class FreeplayState extends MusicBeatState
 
 			
 			// too laggy with a lot of songs, so i had to recode the logic for it
-			songText.visible = songText.active = songText.isMenuItem = false;
+			songText.visible = songText.active = false;
 			icon.visible = icon.active = false;
 
 			// using a FlxGroup is too much fuss!
